67bc904d6681e355955bb2cfe44384aae4fa9e00
simplystudios <new.anshwadhwa@gmail.com>
2026-03-23 14:43:11 +0530

    fixed icons not loading x2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "material-symbols/sharp.css";
+import "material-symbols/rounded.css";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
