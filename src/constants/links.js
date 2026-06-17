2dd655c88578d3ea86440d7b0f4719c83a81a003
Ansh Wadhwa <anshwadhwa@Anshs-MacBook-Air.local>
2026-06-18 01:46:22 +0530

    link now points to the detrogo org

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,5 +1,5 @@
 export const WAITLIST_FORM_URL = "https://forms.gle/69tLCGZrFGDaMBUX7";
-export const GITHUB_REPO_URL = "https://github.com/simplystudios/DetroGo";
+export const GITHUB_REPO_URL = "https://github.com/DetroGO";
 export const GITHUB_PROFILE_URL = "https://github.com/simplystudios";
 export const THREADS_PROFILE_URL = "https://www.threads.com/@anshwadhwa8";
 export const X_PROFILE_URL = "https://x.com/anshwadhwa8";
